d7f1a04f7f056140711641474b54efd4166f8452
Vasil Lozanov <vasillozanov1@gmail.com>
2023-02-14 16:38:28 +0200

    added helper methdos to check for completed bonus

--- backend/cmd/game/bonuscard.go
+++ backend/cmd/game/bonuscard.go
@@ -13,6 +13,38 @@ type Bonus struct {
 }
 
 type BonusMap map[BadgeName]Bonus
+type PowerType int64
+
+const (
+	// Grants no wizard power
+	PWRNoPower PowerType = iota
+
+	//You can play your domino without following
+	// the Connection Rules.
+	PWRNoConnectionRules
+
+	// You can separate your dice to fill in your map.
+	// Each die must respect the Connection Rules.
+	PWRSeparateDominos
+
+	// During a turn where you are the player A,
+	// you can immediately pick your 2 dice
+	PWRPickTwoDice
+
+	// You can turn one of the dice in your
+	// domino around so that it shows any face
+	// of your cho
+	PWRSelectDieSideOfChoice
+
+	// Choose a coat of arms. Each different
+	// DOMAIN with this coat of arms will earn you
+	// 3 prestige points at the end of the game.
+	PWRDomainPoints
+
+	// Add one cross to the coat of arms of your
+	// choosing.
+	PWRAddNoble
+)
 
 func NewBonusMap() *BonusMap {
 	return &BonusMap{
@@ -81,4 +113,30 @@ func (bm *BonusMap) IsThereACompletedBonus() bool {
 	}
 
 	return false
+}
+
+// Returns the badge name that is required for the given power
+func getBonusBadge(pt PowerType) BadgeName {
+	switch pt {
+	case PWRNoConnectionRules:
+		return DOT
+	case PWRSeparateDominos:
+		return LINE
+	case PWRPickTwoDice:
+		return DOUBLEDOT
+	case PWRSelectDieSideOfChoice:
+		return DOUBLEDOT
+	case PWRDomainPoints:
+		return FILLED
+	case PWRAddNoble:
+		return CHECKED
+	default:
+		return EMPTY
+	}
+}
+
+// Returns if the bonus for a specific badge is completed
+func (bm *BonusMap) IsBonusCompleted(pt PowerType) bool {
+	bonus := (*bm)[getBonusBadge(pt)]
+	return bonus.IsCompleted()
 }
