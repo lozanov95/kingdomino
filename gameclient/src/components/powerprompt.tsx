d1ad9b1748ed6d95308217abc4302659fc68a193
Vasil Lozanov <vasillozanov1@gmail.com>
2023-03-13 15:58:17 +0200

    changes on the powerprompt component

--- gameclient/src/components/powerprompt.tsx
+++ gameclient/src/components/powerprompt.tsx
@@ -3,12 +3,12 @@ import { PlayerPower } from "./common";
 export function PowerPrompt(
     { power, handlePowerChoice }: { power: PlayerPower, handlePowerChoice: any }) {
     return (
-        <div>
+        <div className="status">
             <span>
-                Do you want to use the following power: {power.description}?
+                Do you want to use the following power: <p>{power.description}</p>
             </span>
-            <button onClick={() => { handlePowerChoice(true) }}>Yes</button>
-            <button onClick={() => { handlePowerChoice(false) }}>No</button>
+            <button className="font-bold text-xl px-5 mr-5" onClick={() => { handlePowerChoice(true) }}>Yes</button>
+            <button className="font-bold text-xl px-5" onClick={() => { handlePowerChoice(false) }}>No</button>
         </div >
     )
 }
