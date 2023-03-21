a78f16b7985d4c0ace313050d4ea9e8d7bb8a695
Vasil Lozanov <vasillozanov1@gmail.com>
2023-03-21 16:26:10 +0200

    the dice section texts are weighted

--- gameclient/src/components/dice.tsx
+++ gameclient/src/components/dice.tsx
@@ -6,7 +6,7 @@ export const DiceSection = memo(
     function DiceSection({ dices, selectedDice, handleDiceSelect }: { dices: Domino[] | null, selectedDice: Domino[] | null, handleDiceSelect: MouseEventHandler }) {
 
         return (
-            <div className="dice-section">
+            <div className="dice-section font-bold">
                 <div>
                     {dices ? <h2>Available dice</h2> : ""}
                     {dices?.map(({ name, nobles }, idx) => {
@@ -14,7 +14,7 @@ export const DiceSection = memo(
                     })}
                 </div>
                 <div className="dice-selected">
-                    {selectedDice?.length ?? 0 > 0 ? <h2>Selected</h2> : ""}
+                    {selectedDice?.length ?? 0 > 0 ? <h2 className="p-2">Selected</h2> : ""}
                     <Row elements={selectedDice} onClick={handleDiceSelect} />
                 </div>
             </div>
