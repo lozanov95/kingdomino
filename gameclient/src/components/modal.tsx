e09233bb4e79bd5598d70cc47dddcef4c6d950c6
Vasil Lozanov <vasillozanov1@gmail.com>
2023-04-25 01:09:34 +0300

    the modal now blurs the background and prevents accidental clicks on the board/dice

--- gameclient/src/components/modal.tsx
+++ gameclient/src/components/modal.tsx
@@ -6,27 +6,27 @@ export function ModalPrompt({
   onClick: (selection: boolean) => void;
 }) {
   return (
-    <div className="row-start-1 col-start-2 col-end-4 bg-neutral-900 h-fit p-3 rounded-lg m-auto fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] opacity-90">
-      <div>
+    <div className="absolute w-full h-full bg-gray-800 opacity-90 top-0 left-0">
+      <div className="bg-black h-fit p-3 rounded-lg m-auto fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
         <p className="text-center text-xl font-bold">{prompt}</p>
-      </div>
-      <div className="text-center">
-        <button
-          onClick={() => {
-            onClick(true);
-          }}
-          className="bg-blue-800 py-1 px-5 rounded-lg mx-2 mt-3"
-        >
-          Yes
-        </button>
-        <button
-          onClick={() => {
-            onClick(false);
-          }}
-          className="bg-red-800 py-1 px-5 rounded-lg"
-        >
-          No
-        </button>
+        <div className="text-center">
+          <button
+            onClick={() => {
+              onClick(true);
+            }}
+            className="bg-blue-800 py-1 px-5 rounded-lg mx-2 mt-3"
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => {
+              onClick(false);
+            }}
+            className="bg-red-800 py-1 px-5 rounded-lg"
+          >
+            No
+          </button>
+        </div>
       </div>
     </div>
   );
