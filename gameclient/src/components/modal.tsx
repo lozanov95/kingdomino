1af7558d992dc36d57b7019ab562352d38e7edb3
Vasil Lozanov <vasillozanov1@gmail.com>
2023-04-25 11:48:19 +0300

    added modal with children

--- gameclient/src/components/modal.tsx
+++ gameclient/src/components/modal.tsx
@@ -6,27 +6,35 @@ export function ModalPrompt({
   onClick: (selection: boolean) => void;
 }) {
   return (
+    <ModalWithChildren>
+      <p className="text-center text-xl font-bold">{prompt}</p>
+      <div className="text-center">
+        <button
+          onClick={() => {
+            onClick(true);
+          }}
+          className="bg-blue-800 py-1 px-5 rounded-lg mx-2 mt-3"
+        >
+          Yes
+        </button>
+        <button
+          onClick={() => {
+            onClick(false);
+          }}
+          className="bg-red-800 py-1 px-5 rounded-lg"
+        >
+          No
+        </button>
+      </div>
+    </ModalWithChildren>
+  );
+}
+
+export function ModalWithChildren({ children }: { children: React.ReactElement[] }) {
+  return (
     <div className="absolute w-full h-full bg-gray-800 opacity-90 top-0 left-0">
       <div className="bg-black h-fit p-3 rounded-lg m-auto fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-        <p className="text-center text-xl font-bold">{prompt}</p>
-        <div className="text-center">
-          <button
-            onClick={() => {
-              onClick(true);
-            }}
-            className="bg-blue-800 py-1 px-5 rounded-lg mx-2 mt-3"
-          >
-            Yes
-          </button>
-          <button
-            onClick={() => {
-              onClick(false);
-            }}
-            className="bg-red-800 py-1 px-5 rounded-lg"
-          >
-            No
-          </button>
-        </div>
+        {children}
       </div>
     </div>
   );
